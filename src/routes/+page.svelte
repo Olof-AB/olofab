2363983d1fabcdb385c05b91068bc269b08e8bb9
Olof Beckman <olof@olofbeckman.se>
2024-03-28 16:11:56 +0100

    Better spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,30 @@
 <script>
-  import { getWeather } from "$lib/weather";
-  import * as Card from "$lib/components/ui/card";
-  import Line from "$lib/components/viz/line.svelte";
-	import { onMount } from "svelte";
+	import { getWeather } from '$lib/weather';
+	import * as Card from '$lib/components/ui/card';
+	import Line from '$lib/components/viz/line.svelte';
+	import { onMount } from 'svelte';
 
-  let width;
-  let height;
+	let width;
+	let height;
 
-  let data = [];
-  
-  onMount(async () => (
-    data = await getWeather()
-  ))
+	let data = [];
+
+	onMount(async () => (data = await getWeather()));
 </script>
 
-<div class="flex flex-col justify-evenly min-h-screen">
-<div class="flex flex-row justify-evenly items-center">
-  <Card.Root class="max-w-xl md:mr-16">
-    <Card.Content>
-      At Olof AB, we transform complex data into clear, actionable insights, empowering your organization to make informed decisions. Our expertise in IT security and data visualization goes beyond the basics, turning your data into a visual narrative that’s secure and straightforward. This foundation ensures not just the safety of your information but also its effective communication.    </Card.Content>
-  </Card.Root>
-</div>
-<div bind:clientWidth={width} bind:clientHeight={height} class="flex md:m-40" >
-  <Line {data} getX={(d) => d.time} {width} {height} />
-
-</div>
+<div class="flex min-h-screen flex-col justify-evenly">
+	<div class="flex flex-row items-center justify-evenly">
+		<Card.Root class="m-8 max-w-xl md:m-32">
+			<Card.Content>
+				At Olof AB, we transform complex data into clear, actionable insights, empowering your
+				organization to make informed decisions. Our expertise in IT security and data visualization
+				goes beyond the basics, turning your data into a visual narrative that’s secure and
+				straightforward. This foundation ensures not just the safety of your information but also
+				its effective communication.
+			</Card.Content>
+		</Card.Root>
+	</div>
+	<div bind:clientWidth={width} bind:clientHeight={height} class="flex md:ml-20 md:mr-20">
+		<Line {data} getX={(d) => d.time} {width} {height} />
+	</div>
 </div>
