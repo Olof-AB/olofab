377dc910fdb3b8f033aeec7c3c722cd86fed9787
Olof Beckman <olof@olofbeckman.se>
2024-03-28 14:53:06 +0100

    Removed unused stuff for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,20 +12,13 @@
   onMount(async () => (
     data = await getWeather()
   ))
-
-  $: console.log(data);
 </script>
 
 <div class="flex flex-col justify-evenly min-h-screen">
-<div class="flex flex-row justify-evenly items-center space-x-40">
-  <Card.Root class="max-w-xl">
+<div class="flex flex-row justify-evenly items-center">
+  <Card.Root class="max-w-xl md:mr-16">
     <Card.Content>
       At Olof AB, we transform complex data into clear, actionable insights, empowering your organization to make informed decisions. Our expertise in IT security and data visualization goes beyond the basics, turning your data into a visual narrative that’s secure and straightforward. This foundation ensures not just the safety of your information but also its effective communication.    </Card.Content>
-  </Card.Root>
-  <Card.Root class="max-w-sm">
-    <Card.Content>
-      
-    </Card.Content>
   </Card.Root>
 </div>
 <div bind:clientWidth={width} bind:clientHeight={height} class="flex md:m-40" >
