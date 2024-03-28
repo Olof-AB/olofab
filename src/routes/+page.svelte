2b297167cf42f310f444c8b1ac03f19ab190f783
Olof Beckman <olof@olofbeckman.se>
2024-03-28 13:31:14 +0100

    Minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,9 @@
 </script>
 
 <div class="flex flex-row justify-evenly items-center min-h-screen space-x-40">
-  <Card.Root class="max-w-xl border-0">
+  <Card.Root class="max-w-xl">
     <Card.Content>
-      At Olof AB, we transform complex data into clear, actionable insights, empowering your organization to make informed decisions. Our expertise in IT security is not just an add-on; it's our foundation. We understand the importance of safeguarding your data while unlocking its full potential.
-    </Card.Content>
+      At Olof AB, we transform complex data into clear, actionable insights, empowering your organization to make informed decisions. Our expertise in IT security and data visualization goes beyond the basics, turning your data into a visual narrative that’s secure and straightforward. This foundation ensures not just the safety of your information but also its effective communication.    </Card.Content>
   </Card.Root>
   <Card.Root class="max-w-sm">
     <Card.Content>
