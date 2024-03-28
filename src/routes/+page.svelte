226b1da7296089bccf46903076d7f0c2d82be569
Olof Beckman <olof@olofbeckman.se>
2024-03-28 12:52:12 +0100

    Fixed basic colors, added font and some copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="flex flex-row justify-evenly items-center min-h-screen space-x-40">
-  <Card.Root class="max-w-xl">
+  <Card.Root class="max-w-xl border-0">
     <Card.Content>
-      Things are good
+      At Olof AB, we transform complex data into clear, actionable insights, empowering your organization to make informed decisions. Our expertise in IT security is not just an add-on; it's our foundation. We understand the importance of safeguarding your data while unlocking its full potential.
     </Card.Content>
   </Card.Root>
   <Card.Root class="max-w-sm">
