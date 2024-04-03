60e556933615264603bf0235e25271c72a97929f
Olof Beckman <olof@olofbeckman.se>
2024-04-03 14:06:07 +0200

    Added slack bot

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,7 @@ import { superValidate, message } from 'sveltekit-superforms';
 import { formSchema } from '$lib/schema';
 import { zod } from 'sveltekit-superforms/adapters';
 import { getWeather } from '$lib/weather';
+import { env } from '$env/dynamic/private';
 
 export const load: PageServerLoad = async () => {
 	return {
@@ -22,6 +23,18 @@ export const actions: Actions = {
 		}
 
 		console.log('Filled out form: ', form);
+
+		const { emailAddress, name, message: formMessage } = form.data;
+
+		const slack_user = env.SLACK_USER ? `<@${env.SLACK_USER}> ` : '';
+
+		const message_text = `${slack_user}Message from web form: ${name}<${emailAddress}>:\n\n${formMessage}`;
+
+		await fetch(env.SLACK_HOOK, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ text: message_text })
+		});
 
 		// return {
 		// 	form
