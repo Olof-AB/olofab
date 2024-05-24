d8919cd5c3b8d18f91bce7ef7cded72ae009068e
Olof Beckman <olof@olofbeckman.se>
2024-05-24 09:15:23 +0200

    Adding train information

--- src/lib/trains.ts
+++ src/lib/trains.ts
@@ -2,21 +2,56 @@ import { env } from '$env/dynamic/private';
 
 const api_key = env.TRAFIKVERKET_API;
 
+interface TrvTrain {
+	ActivityId: string;
+	ActivityType: string;
+	Advertised: boolean;
+	AdvertisedTimeAtLocation: Date;
+	AdvertisedTrainIdent: string;
+	Canceled: boolean;
+	Deleted: boolean;
+	DepartureDateOTN: string;
+	EstimatedTimeIsPreliminary: boolean;
+	EstimatedTimeAtLocation: Date | undefined;
+	FromLocation: Array<{ LocationName: string; Priority: number; Order: number }>;
+	InformationOwner: string;
+	LocationDateTimeOTN: string;
+	LocationSignature: string;
+	ModifiedTime: string;
+	NewEquipment: number;
+	Operator: string;
+	OperationalTrainNumber: string;
+	OperationalTransportIdentifiers: Array<{ Id: string; Type: string }>;
+	OtherInformation: Array<{ Description: string; Code: string }>;
+	PlannedEstimatedTimeAtLocationIsValid: boolean;
+	ProductInformation: Array<{ Code: string; Description: string }>;
+	ScheduledDepartureDateTime: string;
+	TimeAtLocation: Date;
+	TimeAtLocationWithSeconds: string;
+	ToLocation: Array<{ LocationName: string; Priority: number; Order: number }>;
+	TrackAtLocation: string;
+	TrainOwner: string;
+	TypeOfTraffic: Array<{ Code: string; Description: string }>;
+	ViaFromLocation: Array<{ LocationName: string; Priority: number; Order: number }>;
+	WebLink: string;
+	WebLinkName: string;
+}
+
 export async function get_trains(location_signature: string) {
 	console.log('Getting trains for ', location_signature);
 
 	const question = `<?xml version="1.0" ?>
 		<REQUEST>
-						<LOGIN authenticationkey="${api_key}" />
-						<QUERY objecttype="TrainAnnouncement" schemaversion="1.9" limit="10">
-										<FILTER>
-														<GT name="AdvertisedTimeAtLocation" value="$dateadd(-0.1:00)" />
-														<LT name="AdvertisedTimeAtLocation" value="$now" />
-														<OR>
-																		<EQ name="LocationSignature" value="${location_signature}" />
-														</OR>
-										</FILTER>
-						</QUERY>
+			<LOGIN authenticationkey="${api_key}" />
+			<QUERY objecttype="TrainAnnouncement" schemaversion="1.9" limit="10">
+				<FILTER>
+					<GT name="AdvertisedTimeAtLocation" value="$dateadd(-0.1:00)" />
+					<LT name="AdvertisedTimeAtLocation" value="$now" />
+					<OR>
+						<EQ name="LocationSignature" value="${location_signature}" />
+					</OR>
+				</FILTER>
+			</QUERY>
 		</REQUEST>`;
 
 	const trainResponse = await fetch('https://api.trafikinfo.trafikverket.se/v2/data.json', {
@@ -36,7 +71,15 @@ export async function get_trains(location_signature: string) {
 
 	console.log(trains);
 
-	return trains.map((train: any) => ({
+	return trains.map((train: TrvTrain) => ({
+		planned: new Date(train['AdvertisedTimeAtLocation']),
 		estimated: train['EstimatedTimeAtLocation']
+			? new Date(train['EstimatedTimeAtLocation'])
+			: undefined,
+		destination: train['ToLocation']
+			? train['ToLocation'].map((loc) => loc['LocationName']).join(', ')
+			: 'Unknown',
+		platform: train['TrackAtLocation'],
+		operator: train['Operator']
 	}));
 }
--- src/routes/trains/+page.svelte
+++ src/routes/trains/+page.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
+	import * as Card from '$lib/components/ui/card';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	const trains = data.trains;
 </script>
 
-<h1>Trains</h1>
-
-{#each trains as train}
-	<p>{train.estimated}</p>
-{/each}
+<div class="flex min-h-screen flex-row justify-start">
+	<div class="flex flex-col">
+		{#each trains as train}
+			{#if train.operator === 'MTRN'}
+				<Card.Root class="max-w-xlx m-8">
+					<Card.Content>
+						<p>{train.planned.toLocaleTimeString()}</p>
+						{#if train.estimated}
+							<p>{train.estimated.toLocaleTimeString()}</p>
+						{:else}
+							<p>On time</p>
+						{/if}
+						<p>{train.platform}</p>
+						<p>{train.destination}</p>
+					</Card.Content>
+				</Card.Root>
+			{/if}
+		{/each}
+	</div>
+</div>
